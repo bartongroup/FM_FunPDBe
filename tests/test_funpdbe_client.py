a70ab0bd07f396e8784889285c2e94db8e0509f7
Mihaly Varadi <mvaradi@ebi.ac.uk>
2018-02-06 10:44:41 +0000

    Changing license from MIT to EMBL-EBI Apache 2.0

--- tests/test_funpdbe_client.py
+++ tests/test_funpdbe_client.py
@@ -1,6 +1,18 @@
+# Copyright 2018 EMBL - European Bioinformatics Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific
+# language governing permissions and limitations under the
+# License.
+
 import unittest
 from client.funpdbe_client import User
-
 
 class TestUser(unittest.TestCase):
 
@@ -9,51 +21,3 @@ class TestUser(unittest.TestCase):
 
     def test_setting_user_name(self):
         self.assertIsNotNone(self.user.user_name)
-
-
-
-#
-# example_json = {
-#     "pdb_id": "1abc",
-#     "data_resource": "funsites",
-#     "resource_version": "1.0.0",
-#     "software_version": "1.0.0",
-#     "resource_entry_url": "https://example.com/foo/bar",
-#     "release_date": "01/01/2000",
-#     "chains": [
-#         {
-#             "chain_label": "A",
-#             "chain_annotation": "foo",
-#             "residues": [
-#                 {
-#                     "pdb_res_label": "1",
-#                     "aa_type": "A",
-#                     "site_data": [
-#                         {
-#                             "site_id_ref": 1,
-#                             "value": 0.7,
-#                             "confidence": 0.9,
-#                             "classification": "reliable"
-#                         }
-#                     ]
-#                 }
-#             ]
-#         }
-#     ],
-#     "sites": [
-#         {
-#             "site_id": 1,
-#             "label": "ligand_binding_site",
-#             "source_database": "pdb",
-#             "source_accession": "1ABC",
-#             "source_release_date": "01/01/2000"
-#         }
-#     ],
-#     "evidence_code_ontology": [
-#         {
-#             "eco_term": "manually_curated"
-#         }
-#     ]
-# }
-
-
