7f56fa638efcd5622d6a3c1b709dd04d24ff9efd
Mihaly Varadi <mvaradi@ebi.ac.uk>
2018-02-06 14:28:45 +0000

    Unit tests

--- tests/test_funpdbe_client.py
+++ tests/test_funpdbe_client.py
@@ -12,7 +12,28 @@
 # License.
 
 import unittest
+from client.funpdbe_client import Schema
 from client.funpdbe_client import User
+from client.funpdbe_client import Client
+
+
+class TestSchema(unittest.TestCase):
+
+    def setUp(self):
+        self.schema = Schema()
+
+    def test_validate_json_with_missing_schema(self):
+        self.schema.json_schema = ""
+        self.assertFalse(self.schema.validate_json({"foo": "bar"}))
+
+    def test_validate_json_with_missing_data(self):
+        self.schema.json_schema = {"foo": "bar"}
+        self.assertFalse(self.schema.validate_json(None))
+
+    def test_validate_json(self):
+        self.schema.json_schema = {"foo": "bar"}
+        self.assertTrue(self.schema.validate_json({"foo": "bar"}))
+
 
 class TestUser(unittest.TestCase):
 
@@ -21,3 +42,15 @@ class TestUser(unittest.TestCase):
 
     def test_setting_user_name(self):
         self.assertIsNotNone(self.user.user_name)
+
+    def test_setting_password(self):
+        self.assertIsNotNone(self.user.user_pwd)
+
+
+class TestClient(unittest.TestCase):
+
+    def setUp(self):
+        self.client = Client("user", "pwd")
+
+    def test_parse_missing_json(self):
+        self.assertFalse(self.client.parse_json(""))
