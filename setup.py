828dd6bdef82b8864cadb0b5818dfee70d5b54b4
Mihaly Varadi <mvaradi@ebi.ac.uk>
2018-02-19 11:04:32 +0000

    Updating setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open('README.rst') as f:
     readme = f.read()
@@ -7,13 +7,16 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='funpdbe-client',
-    version='0.1.0',
+    name='funpdbe_client',
+    version='1.0.0',
     description='Deposition client for FunPDBe',
     long_description=readme,
     author='Mihaly Varadi',
     author_email='mvaradi@ebi.ac.uk',
-    url='https://github.com/mvaradi/funpdbe-client',
+    url='https://github.com/funpdbe-consortium/funpdbe-client',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=['funpdbe_client'],
+    install_requires=[
+        'jsonschema'
+    ]
 )
