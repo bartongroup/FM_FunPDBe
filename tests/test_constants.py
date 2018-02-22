739768c115740433196265a3ae515dedda204549
Mihaly Varadi <mvaradi@ebi.ac.uk>
2018-02-22 14:57:51 +0000

    Testing Constants()

---
+++ tests/test_constants.py
@@ -0,0 +1,17 @@
+import unittest
+from funpdbe_client.constants import Constants
+
+
+class TestConstants(unittest.TestCase):
+
+    def setUp(self):
+        self.constants = Constants()
+
+    def test_get_api_url(self):
+        self.assertIsNotNone(self.constants.get_api_url())
+
+    def test_get_resources(self):
+        self.assertIsNotNone(self.constants.get_resources())
+
+    def test_get_pattern(self):
+        self.assertIsNotNone(self.constants.get_pdb_id_pattern())
