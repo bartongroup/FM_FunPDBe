0961667cad080dc6129c4f6042c5e688f28406f0
Mihaly Varadi <mvaradi@ebi.ac.uk>
2018-02-23 09:43:13 +0000

    Updating tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -6,16 +6,17 @@ from funpdbe_client.user import User
 class TestUser(TestCase):
 
     def setUp(self):
-        self.user = User("foo", "bar")
+        self.user = User('foo', 'bar')
 
     def test_set_user(self):
-        self.user.user_name = None
-        with patch('builtins.input', side_effect="f"):
-            self.user.set_user()
-        self.assertEqual("f", self.user.user_name)
+        self.user.set_user()
+        self.assertEqual('foo', self.user.user_name)
 
     def test_set_pwd(self):
-        self.user.user_pwd = None
-        with patch('builtins.input', side_effect="f"):
-            self.user.set_pwd()
-        self.assertEqual("f", self.user.user_pwd)
+        self.user.set_pwd()
+        self.assertEqual('bar', self.user.user_pwd)
+
+    def test_set_attribute(self):
+        with patch('builtins.input', side_effect='f'):
+            new_value = self.user.set_attribute(None, 'user prompt message')
+        self.assertEqual('f', new_value)
