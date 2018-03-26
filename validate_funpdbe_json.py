ee4ea5cd9ea7b52c673359db42b75f94c39d55be
Mihaly Varadi <mvaradi@ebi.ac.uk>
2018-03-26 15:31:19 +0100

    Releasing v1.0.0

--- validate_funpdbe_json.py
+++ validate_funpdbe_json.py
@@ -1,7 +1,7 @@
 import jsonschema
 import json
 
-with open("funpdbe_schema.v0.0.1.json") as schema_file:
+with open("funpdbe_schema.v1.0.0.json") as schema_file:
     funpdbe_schema = json.load(schema_file)
 
 with open("funpdbe_example.json") as example_file:
